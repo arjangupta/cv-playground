fdb2c3874431eb9201b7e775ec2f404557b0139f
Arjan Gupta <arjangupta@vermeer.com>
2025-01-04 11:36:35 -0600

    Add code to exercise identity function

--- resnet_identity_block.py
+++ resnet_identity_block.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-import scipy.misc
+# import scipy.misc
 from tensorflow.keras.applications.resnet_v2 import ResNet50V2
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet_v2 import preprocess_input, decode_predictions
@@ -8,14 +8,14 @@ from tensorflow.keras import layers
 from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.models import Model, load_model
-from resnets_utils import *
+# from resnets_utils import *
 from tensorflow.keras.initializers import random_uniform, glorot_uniform, constant, identity
 from tensorflow.python.framework.ops import EagerTensor
 from matplotlib.pyplot import imshow
 
 
-from test_utils import summary, comparator
-import public_tests
+# from test_utils import summary, comparator
+# import public_tests
 
 # %matplotlib inline
 np.random.seed(1)
@@ -64,3 +64,30 @@ def identity_block(X, f, filters, initializer=random_uniform):
     ### END CODE HERE
 
     return X
+
+tf.keras.backend.set_learning_phase(False)
+
+np.random.seed(1)
+tf.random.set_seed(2)
+X1 = np.ones((1, 4, 4, 3)) * -1
+X2 = np.ones((1, 4, 4, 3)) * 1
+X3 = np.ones((1, 4, 4, 3)) * 3
+
+X = np.concatenate((X1, X2, X3), axis = 0).astype(np.float32)
+
+A3 = identity_block(X, f=2, filters=[4, 4, 3],
+                   initializer=lambda seed=0:constant(value=1))
+print('\033[1mWith training=False\033[0m\n')
+A3np = A3.numpy()
+print(np.around(A3.numpy()[:,(0,-1),:,:].mean(axis = 3), 5))
+resume = A3np[:,(0,-1),:,:].mean(axis = 3)
+print(resume[1, 1, 0])
+
+tf.keras.backend.set_learning_phase(True)
+
+print('\n\033[1mWith training=True\033[0m\n')
+np.random.seed(1)
+tf.random.set_seed(2)
+A4 = identity_block(X, f=2, filters=[3, 3, 3],
+                   initializer=lambda seed=0:constant(value=1))
+print(np.around(A4.numpy()[:,(0,-1),:,:].mean(axis = 3), 5))
