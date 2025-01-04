6ce96687362aa0b4edc630fb0cbe871e9635bb86
Arjan Gupta <arjangupta@vermeer.com>
2025-01-04 11:23:59 -0600

    Add resnet identity block script

---
+++ resnet_identity_block.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import numpy as np
+import scipy.misc
+from tensorflow.keras.applications.resnet_v2 import ResNet50V2
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet_v2 import preprocess_input, decode_predictions
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Model, load_model
+from resnets_utils import *
+from tensorflow.keras.initializers import random_uniform, glorot_uniform, constant, identity
+from tensorflow.python.framework.ops import EagerTensor
+from matplotlib.pyplot import imshow
+
+
+from test_utils import summary, comparator
+import public_tests
+
+# %matplotlib inline
+np.random.seed(1)
+tf.random.set_seed(2)
+
+def identity_block(X, f, filters, initializer=random_uniform):
+    """
+    Implementation of the identity block as defined in Figure 4
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    initializer -- to set up the initial weights of a layer. Equals to random uniform initializer
+    
+    Returns:
+    X -- output of the identity block, tensor of shape (m, n_H, n_W, n_C)
+    """
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value. You'll need this later to add back to the main path. 
+    X_shortcut = X
+    
+    # First component of main path
+    X = Conv2D(filters = F1, kernel_size = 1, strides = (1,1), padding = 'valid', kernel_initializer = initializer(seed=0))(X)
+    X = BatchNormalization(axis = 3)(X) # Default axis
+    X = Activation('relu')(X)
+    
+    ### START CODE HERE
+    ## Second component of main path (≈3 lines)
+    ## Set the padding = 'same'
+    X = Conv2D(filters=F2, kernel_size=f, strides=(1,1), padding='same', kernel_initializer= initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X)
+    X = Activation('relu')(X)
+
+    ## Third component of main path (≈2 lines)
+    ## Set the padding = 'valid'
+    X = Conv2D(filters=F3, kernel_size=1, strides=(1,1), padding='valid', kernel_initializer= initializer(seed=0))(X)
+    X = BatchNormalization(axis=3) 
+
+    ## Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = Add()([X,X_shortcut])
+    X = Activation('relu')(X)
+    ### END CODE HERE
+
+    return X
