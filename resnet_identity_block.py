d018f1aa9723164e85fb0e1f23e5ba0cbd6dd315
Arjan Gupta <arjangupta@vermeer.com>
2025-01-04 11:38:48 -0600

    Fix code to not use deprecated calls

--- resnet_identity_block.py
+++ resnet_identity_block.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-# import scipy.misc
 from tensorflow.keras.applications.resnet_v2 import ResNet50V2
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet_v2 import preprocess_input, decode_predictions
@@ -8,20 +7,13 @@ from tensorflow.keras import layers
 from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.models import Model, load_model
-# from resnets_utils import *
 from tensorflow.keras.initializers import random_uniform, glorot_uniform, constant, identity
-from tensorflow.python.framework.ops import EagerTensor
 from matplotlib.pyplot import imshow
 
-
-# from test_utils import summary, comparator
-# import public_tests
-
-# %matplotlib inline
 np.random.seed(1)
 tf.random.set_seed(2)
 
-def identity_block(X, f, filters, initializer=random_uniform):
+def identity_block(X, f, filters, initializer=random_uniform, training=False):
     """
     Implementation of the identity block as defined in Figure 4
     
@@ -30,6 +22,7 @@ def identity_block(X, f, filters, initializer=random_uniform):
     f -- integer, specifying the shape of the middle CONV's window for the main path
     filters -- python list of integers, defining the number of filters in the CONV layers of the main path
     initializer -- to set up the initial weights of a layer. Equals to random uniform initializer
+    training -- boolean, whether the model is in training mode or not
     
     Returns:
     X -- output of the identity block, tensor of shape (m, n_H, n_W, n_C)
@@ -42,30 +35,24 @@ def identity_block(X, f, filters, initializer=random_uniform):
     X_shortcut = X
     
     # First component of main path
-    X = Conv2D(filters = F1, kernel_size = 1, strides = (1,1), padding = 'valid', kernel_initializer = initializer(seed=0))(X)
-    X = BatchNormalization(axis = 3)(X) # Default axis
+    X = Conv2D(filters=F1, kernel_size=1, strides=(1,1), padding='valid', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training) # Default axis
     X = Activation('relu')(X)
     
-    ### START CODE HERE
-    ## Second component of main path (≈3 lines)
-    ## Set the padding = 'same'
-    X = Conv2D(filters=F2, kernel_size=f, strides=(1,1), padding='same', kernel_initializer= initializer(seed=0))(X)
-    X = BatchNormalization(axis=3)(X)
+    # Second component of main path
+    X = Conv2D(filters=F2, kernel_size=f, strides=(1,1), padding='same', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)
     X = Activation('relu')(X)
 
-    ## Third component of main path (≈2 lines)
-    ## Set the padding = 'valid'
-    X = Conv2D(filters=F3, kernel_size=1, strides=(1,1), padding='valid', kernel_initializer= initializer(seed=0))(X)
-    X = BatchNormalization(axis=3)(X)
+    # Third component of main path
+    X = Conv2D(filters=F3, kernel_size=1, strides=(1,1), padding='valid', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)
     
-    ## Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
-    X = Add()([X,X_shortcut])
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation
+    X = Add()([X, X_shortcut])
     X = Activation('relu')(X)
-    ### END CODE HERE
 
     return X
-
-tf.keras.backend.set_learning_phase(False)
 
 np.random.seed(1)
 tf.random.set_seed(2)
@@ -73,21 +60,17 @@ X1 = np.ones((1, 4, 4, 3)) * -1
 X2 = np.ones((1, 4, 4, 3)) * 1
 X3 = np.ones((1, 4, 4, 3)) * 3
 
-X = np.concatenate((X1, X2, X3), axis = 0).astype(np.float32)
+X = np.concatenate((X1, X2, X3), axis=0).astype(np.float32)
 
-A3 = identity_block(X, f=2, filters=[4, 4, 3],
-                   initializer=lambda seed=0:constant(value=1))
 print('\033[1mWith training=False\033[0m\n')
+A3 = identity_block(X, f=2, filters=[4, 4, 3], initializer=lambda seed=0: constant(value=1), training=False)
 A3np = A3.numpy()
-print(np.around(A3.numpy()[:,(0,-1),:,:].mean(axis = 3), 5))
-resume = A3np[:,(0,-1),:,:].mean(axis = 3)
+print(np.around(A3.numpy()[:, (0, -1), :, :].mean(axis=3), 5))
+resume = A3np[:, (0, -1), :, :].mean(axis=3)
 print(resume[1, 1, 0])
-
-tf.keras.backend.set_learning_phase(True)
 
 print('\n\033[1mWith training=True\033[0m\n')
 np.random.seed(1)
 tf.random.set_seed(2)
-A4 = identity_block(X, f=2, filters=[3, 3, 3],
-                   initializer=lambda seed=0:constant(value=1))
-print(np.around(A4.numpy()[:,(0,-1),:,:].mean(axis = 3), 5))
+A4 = identity_block(X, f=2, filters=[3, 3, 3], initializer=lambda seed=0: constant(value=1), training=True)
+print(np.around(A4.numpy()[:, (0, -1), :, :].mean(axis=3), 5))
