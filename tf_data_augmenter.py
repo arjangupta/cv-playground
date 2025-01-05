c8c50216740d22fef631c26ae64bbb27a9ff99cd
Arjan Gupta <arjangupta@vermeer.com>
2025-01-05 01:18:08 -0600

    Add data augmenter

---
+++ tf_data_augmenter.py
@@ -0,0 +1,24 @@
+def data_augmenter():
+    '''
+    Create a Sequential model composed of 2 layers
+    Returns:
+        tf.keras.Sequential
+    '''
+    ### START CODE HERE
+    data_augmentation = tf.keras.Sequential([])
+    data_augmentation.add(RandomFlip('horizontal'))
+    data_augmentation.add(RandomRotation(0.2))
+    ### END CODE HERE
+    
+    return data_augmentation
+
+data_augmentation = data_augmenter()
+
+for image, _ in train_dataset.take(1):
+    plt.figure(figsize=(10, 10))
+    first_image = image[0]
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        augmented_image = data_augmentation(tf.expand_dims(first_image, 0))
+        plt.imshow(augmented_image[0] / 255)
+        plt.axis('off')
